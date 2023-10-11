aa385c8becaa5de07129a58b5b85eb360e715854
arilibbetardanaputra <arilardana111@gmail.com>
2023-10-11 13:31:25 +0700

    Daspro Jobsheet 7

--- ForKelipatan05.java
+++ ForKelipatan05.java
@@ -1,9 +1,8 @@
 import java.util.Scanner;
-
 public class ForKelipatan05 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        int kelipatan, total = 0, counter = 0;
+        int kelipatan, total = 0, counter = 0, rata;
     
         System.out.print("Masukkan bilangan kelipatan 1-9 : ");
         kelipatan = scan.nextInt();
@@ -13,7 +12,9 @@ public class ForKelipatan05 {
                 counter++;
             }
         }
+        rata = total / counter;
         System.out.printf("Banyaknya bilangan %d dari 1 sampai 50 adalah %d\n", kelipatan, counter);
         System.out.printf("Total bilangan kelipatan %d dari 1 sampai 50 adalah %d\n", kelipatan, total);
+        System.out.printf("Rata-rata bilangan kelipatan %d dari 1 sampai 50 adalah %.2f\n", kelipatan, rata);
     }
 }
