d546d18d33ba83baab1f4f970f58462090e435d9
arilibbetardanaputra <arilardana111@gmail.com>
2023-10-11 13:57:57 +0700

    Daspro Jobsheet 7

--- DoWhileCuti05.java
+++ DoWhileCuti05.java
@@ -1,28 +1,23 @@
 import java.util.Scanner;
-
 public class DoWhileCuti05 {
        public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int jatahCuti, jumlahHari;
         String konfirmasi;
-
         System.out.print("Jatah cuti : ");
         jatahCuti = sc.nextInt();
-
         do {
             System.out.print("Apakah anda ingin mengambil cuti (y/t)?  ");
             konfirmasi = sc.next();
             if (konfirmasi.equalsIgnoreCase("y")){
                 System.out.print("Jumlah hari : ");
                 jumlahHari = sc.nextInt();
-
-                if (jumlahHari <= jatahCuti) {
-                    jatahCuti -= jumlahHari;
-                    System.out.println("Sisa jatah cuti : " + jatahCuti);
-                } else {
-                    System.out.println("Sisa jatah cuti anda tidak mencukupi");
-                    break;
+                while (jumlahHari > jatahCuti) {
+                    System.out.println("Jumlah hari yang anda masukkan melebihi jatah cuti yang tersedia. Silakan masukkan kembali jumlah hari yang sesuai.");
+                    jumlahHari = sc.nextInt();
                 }
+                jatahCuti -= jumlahHari;
+                System.out.println("Sisa jatah cuti : " + jatahCuti);
             }
         } while(jatahCuti > 0);
     }
