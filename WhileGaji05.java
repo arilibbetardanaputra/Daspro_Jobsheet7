50eb757d7c1e313f59144e76d47cbaa5918384f6
arilibbetardanaputra <arilardana111@gmail.com>
2023-10-11 13:50:13 +0700

    Daspro Jobsheet 7

--- WhileGaji05.java
+++ WhileGaji05.java
@@ -5,12 +5,9 @@ public class WhileGaji05 {
         int jumlahKaryawan, jumlahJamLembur;
         double gajiLembur= 0, totalGajiLembur= 0;
         String jabatan;
-
         System.out.print("Masukkan jumlah karyawan : ");
         jumlahKaryawan = scan.nextInt();
-
         int i = 0;
-
         while (i < jumlahKaryawan){
             System.out.println("Pilihan Jabatan - Direktur, Manajer, Karyawan");
             System.out.print("Masukkan jabatan karyawan ke-" + (i+1) + ": ");
@@ -18,13 +15,16 @@ public class WhileGaji05 {
             System.out.print("Masukkan jam lembur: ");
             jumlahJamLembur = scan.nextInt();
             i++;   
-
             if (jabatan.equalsIgnoreCase("Direktur")) {
                 continue;
             } else if (jabatan.equalsIgnoreCase("Manajer")) {
                 gajiLembur = jumlahJamLembur * 100000;
             } else if (jabatan.equalsIgnoreCase("Karyawan")) {
                 gajiLembur = jumlahJamLembur * 75000;
+            } else {
+                System.out.println("Jabatan tidak valid. Masukkan jabatan yang valid.");
+                i--;
+                continue;
             }
             totalGajiLembur += gajiLembur;
             System.out.println("Total gaji lembur: " + totalGajiLembur);
